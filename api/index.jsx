33927e9e47666dc18105843ba2739782b9289a2f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-29 19:51:34 -0300

    feat: Rota de cadastrar produtos

--- api/index.jsx
+++ api/index.jsx
@@ -1,27 +1,41 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const {v4: uuidv4} = require("uuid");
 
 const app = express();
 const Port = 5001;
 
-app.get("/teste", (req, res) => {
-    res.send("Seja bem vindo ao teste")
-})
+app.use(cors());
+app.use(express.json())
 
-app.get("/login", (req, res) => {
-    res.send("Endpoint de login")
-})
+let produtos = [];
 
-app.get("/produto", (req, res) => {
-    res.send("Nossos produtos")
-})
+// app.get("/teste", (req, res) => {
+//     res.send("Seja bem vindo ao teste")
+// })
 
-app.get("/perfil", (req, res) => {
-    res.send("Perfil do usuário")
-})
+// app.get("/login", (req, res) => {
+//     res.send("Endpoint de login")
+// })
+
+// app.get("/produto", (req, res) => {
+//     res.send("Nossos produtos")
+// })
+
+// app.get("/perfil", (req, res) => {
+//     res.send("Perfil do usuário")
+// })
 
 //ROTA PRODUTO - CREATE
+app.post("/produtos", (req, res) => {
+    const {nome, descricao} = req.body;
+    if (!nome || !descricao) {
+        return res.status(400).json({error: "Campos obrigatórios"});
+    }
+    const novoItem = {id:uuidv4(), nome, descricao};
+    produtos.push(novoItem);
+    res.status(201).json(novoItem)
+})
 
 
 app.listen(Port, () => {
